efc4199e7deb146b90cda35b129203792f9fb398
Syed Fahad <fahadshahf31@gmail.com>
2025-08-13 15:41:19 +0500

    props driling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Card from '../Card'
 const App = () => {
-  return (
-    <div>
-        <Navbar logo={"SHAH"}/>
+  const users = [
+  {
+    "username": "ali_khan",
+    "city": "Lahore",
+    "age": 28,
+    "profession": "Software Engineer"
+  },
+  {
+    "username": "sana_ahmed",
+    "city": "Karachi",
+    "age": 32,
+    "profession": "Graphic Designer"
+  },
+  {
+    "username": "usman_raza",
+    "city": "Islamabad",
+    "age": 24,
+    "profession": "Digital Marketer"
+  },
+  {
+    "username": "fatima_qureshi",
+    "city": "Multan",
+    "age": 30,
+    "profession": "Doctor"
+  },
+  {
+    "username": "hamza_mir",
+    "city": "Faisalabad",
+    "age": 27,
+    "profession": "Civil Engineer"
+  }
+  ]
 
-        <Footer/>
+  return (
+    <div className='comp'>
+      {users.map(function(elem,idx){
+        return <Card key={idx} name={elem.username} city={elem.city} age={elem.age} prof={elem.profession}/>
+      })}
     </div>
   )
 }
+
 export default App
